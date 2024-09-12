faea93456faefa51d33ab181704c7273c6bc6c55
Anthony-Castleberry <castleberryar0425@gmail.com>
2024-09-12 13:31:00 -0500

    Third Commit

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -12,5 +12,5 @@ public class AllCaesar {
     }
     pen.close();
   }
-} // mark
+} // STUB
 
--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -5,10 +5,10 @@ import java.io.PrintWriter;
 public class Cipher {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    for (int i = 0; i < args.length; i++) {
-      pen.printf("args[%d] = \"%s\"\n", i, args[i]);
+    for (int j = 0; j < args.length; j++) {
+      pen.printf("args[%d] = \"%s\"\n", j, args[j]);
     }
     pen.close();
     System.err.println("Error: Invalid parameters");
   }
-}
+} // m

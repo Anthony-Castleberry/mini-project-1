84c57641e70fbbd2a8b8d515b2b21dfa0d4f68c7
Anthony-Castleberry <castleberryar0425@gmail.com>
2024-09-16 22:28:06 -0500

    final

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -3,26 +3,30 @@ package edu.grinnell.csc207.main;
 import java.io.PrintWriter;
 import edu.grinnell.csc207.util.CipherUtils;
 
+/** prints all the ceaserciphers possible using each indivdual lower case letter as a key.*/
 public class AllCaesar {
+  /** runs the code to print out all the ceaserciphers.
+   * @param args strings that correspond to text and encode/decode.
+  */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     if (args.length != 2) {
-      System.err.println("wrong number of parameters");
-    }
+      System.err.println([]);
+    } // if
     if (args[0] != "encode" && args[0] != "decode") {
-      System.err.println("Invalid option:" + args[0] + "valid options are either 'encode' or 'decode'");
-    }
+      System.err.println("Invalid option:" + args[0] + "valid options are 'encode' or 'decode'");
+    } // if
 
-    if(args[0] == "encode") {
+    if (args[0] == "encode") {
       for (char ch = 'a'; ch <= 'z'; ch++) {
         pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(args[1], ch));
-      }
+      } // for
     } else {
       for (char ch = 'a'; ch <= 'z'; ch++) {
         pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(args[1], ch));
-      }
-    }
-    pen.close();
-  }
-}
+      } // for
+    } // if
 
+    pen.close();
+  } // main
+} // Allceaser

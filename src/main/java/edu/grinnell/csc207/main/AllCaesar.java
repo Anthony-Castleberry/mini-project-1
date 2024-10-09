b553ee20c4bf17f26baca07845c489550a86c60d
Anthony Castleberry <castleberryar0425@gmail.com>
2024-10-08 22:10:04 -0500

    Update AllCaesar.java

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -5,10 +5,10 @@ import edu.grinnell.csc207.util.CipherUtils;
 
 /**
  * A project for CSC-207 2024fa
- *  
- * Prints all the ceaserciphers possible using 
+ *
+ * Prints all the ceaserciphers possible using
  * each indivdual lower case letter as a key.
- * 
+ *
  * @author Anthony Castleberry
  */
 public class AllCaesar {
@@ -18,13 +18,11 @@ public class AllCaesar {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     if (args.length != 2) {
-      System.err.println("not enough arguments");
-    } // if
-    if (args[0] != "encode" && args[0] != "decode") {
-      System.err.println("Invalid option:" + args[0] + "valid options are 'encode' or 'decode'");
-    } // if
-
-    if (args[0] == "encode") {
+      System.err.println("Error: not enough arguments");
+    } else if (args[0] != "encode" && args[0] != "decode") {
+      System.err.println("Error: invalid option:" + args[0]
+        + "valid options are 'encode' or 'decode'");
+    } else if (args[0] == "encode") {
       for (char ch = 'a'; ch <= 'z'; ch++) {
         pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(args[1], ch));
       } // for
